52ef7cd05d8042e97959092ec06b3d3ab9f3dc46
Saint-spec <christiancervantes02@gmail.com>
2023-07-04 12:02:34 -0400

    added some basic styling and github link

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 function appendToResult(value) {
-  document.getElementById("result").value += value;
+  const resultInput = document.getElementById("result");
+  const currentResult = resultInput.value;
+  const lastChar = currentResult[currentResult.length - 1];
+
+  if (isNumber(value)) {
+    resultInput.value += value;
+  } else if (isOperator(value) && isNumber(lastChar)) {
+    resultInput.value += value;
+  } else if (value === '.' && isValidDecimal(currentResult)) {
+    resultInput.value += value;
+  }
 }
+
+function isNumber(value) {
+  return !isNaN(parseFloat(value)) && isFinite(value);
+}
+
+function isOperator(value) {
+  return value === '+' || value === '-' || value === '*' || value === '/';
+}
+
+function isValidDecimal(result) {
+  const lastOperatorIndex = Math.max(
+    result.lastIndexOf('+'),
+    result.lastIndexOf('-'),
+    result.lastIndexOf('*'),
+    result.lastIndexOf('/')
+  );
+  const decimalIndex = result.indexOf('.', lastOperatorIndex);
+
+  return decimalIndex === -1;
+}
+
 
 function clearResult() {
   document.getElementById("result").value = "";
